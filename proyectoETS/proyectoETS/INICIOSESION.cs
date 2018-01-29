dfb19e750dcb4cf34fb2c5733700f8b9849242cd
Juanky <juanito_123_nino@hotmail.com>
2018-01-29 14:35:17 -0600

    SALTO DE LINEA

---
+++ proyectoETS/proyectoETS/INICIOSESION.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoETS
+{
+    public partial class INICIOSESION : Form
+    {
+        public INICIOSESION()
+        {
+            InitializeComponent();
+        }
+
+        private void INICIOSESION_Load(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
